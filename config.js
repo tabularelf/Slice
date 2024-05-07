6e740a2244cb0d3fb06aa7599483ce30c25069c1
tabularelf <tabularelf@gmail.com>
2024-05-07 10:55:12 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
 	name: 'Slice',
-	description: 'Layer assistance library for GameMaker LTS',
+	description: 'Layer wrapper library for GameMaker LTS',
 	latestVersion: '1.0.0',
 	otherVersions: [],
 	favicon: 'assets/favicon.ico',
